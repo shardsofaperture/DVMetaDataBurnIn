e5fac5a6a0a9fe82a34ea87303f1a9b9cdfdeb1e
ThePolish <shardsofaperture@gmail.com>
2025-12-05 16:29:12 -0500

    Changed working in changelog etc

--- SplashContentView.swift
+++ SplashContentView.swift
@@ -26,7 +26,7 @@ struct SplashContentView: View {
 
                     HStack(spacing: 16) {
                         Text("© 2025 The Polish")
-                        Text("Version 1.0B")
+                        Text("Version 1.0.1B")
                     }
                     .font(.footnote)
                     .foregroundStyle(.secondary)
